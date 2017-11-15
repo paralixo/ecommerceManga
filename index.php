29e975824e08ae6e2fc23be54264def3e1fccf60
Florian <florian.lafuente@ynov.com>
2017-11-15 14:21:46 +0100

    Ceci est mon commentaire

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ceci est un titre</title>
+        <meta charset="utf-8" />
+        <meta content="description" />
+        <link rel="stylesheet" type="text/css" src="styles.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <p>ceci est un test bidon</p>
+    </body>
+</html>
