c22c066343e50be7b417a877f7d0a09de194623c
Florian <florian.lafuente@ynov.com>
2017-11-20 11:27:24 +0100

    menu.php

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title>Menu de la page</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css">
+        
+    </head>
+    <body>
+        
+        <header>
+              
+        </header>
+
+
+        
+    </body>
+</html>
