a1774fe7a83993be9e60449c806e9bab239b9500
lukihd <lucas.erisset@ynov.com>
2017-11-20 11:41:08 +0100

    footerv2

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- Footer -->
+
+<!-- lien avec la feuille de style -->
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<!-- Block -->
+<!-- Premier block "Qui sommes nous ?" -->
+<div id="block1">
+	
+</div>
+
+<?php
+include(menu.php)
+?>
