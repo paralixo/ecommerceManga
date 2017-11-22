6bcd72c3ec508a2050ed6cd3bc84851c3d65547d
lukihd <lucas.erisset@ynov.com>
2017-11-22 17:34:38 +0100

    police

--- footer.php
+++ footer.php
@@ -3,12 +3,10 @@
 <!-- lien avec la feuille de style -->
 <link rel="stylesheet" type="text/css" href="style.css">
 
-<!-- Block -->
-<!-- Premier block "Qui sommes nous ?" -->
-<div id="block1">
-	
+<!-- Block entier -->
+<div id="footer">
+	<!-- Premier block "Qui sommes nous ?" -->
+	<div id="block1">
+		<p> Nous sommes deux étudiants qui ont pour projet de créer un site d'e-commerce.   
+	</div>
 </div>
-
-<?php
-include(menu.php)
-?>
--- menu.php
+++ menu.php
@@ -5,7 +5,7 @@
         <title>Menu de la page</title>
         <meta charset="utf-8" />
         <link rel="stylesheet" type="text/css" href="style.css">
-        
+      	<link href="https://fonts.googleapis.com/css?family=Poiret+One|Quicksand" rel="stylesheet"> 
     </head>
     <body>
         
@@ -14,10 +14,10 @@
             <img id="logo" src="images/renard.png" />
             <nav>
                 <ul>
-                    <li><a href="#">Acceuil</a></li>
+                    <li><a href="#">Produits Dérivés</a></li>
                     <li><a href="#">Mangas</a></li>
-                    <li><a href="#">Produits dérivés</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Acceuil</a></li>
+                    <li><a href="#">Contacter</a></li>
                     <li><a href="#">Mon compte</a></li>
                 </ul>
             </nav>
