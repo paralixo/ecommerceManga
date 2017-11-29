b640aeb2da75f4ee93f970ac6405313373073078
lukihd <lucas.erisset@ynov.com>
2017-11-29 14:13:30 +0100

    icone

--- easymanga/pages/footer.php
+++ easymanga/pages/footer.php
@@ -10,18 +10,22 @@
 	<!-- Premier block "Qui sommes nous ?" -->
 	<div id="footer_block_left">
 		<h2>
+			Suivez-nous
+		</h2>
+		<a href="www.twitter.com"><img src="../images/logo_twitter" alt="suivez-nous sur twitter" title="Twitter"></a>
+		<a href="www.facebook.com"><img src="../images/logo_facebook" alt="suivez-nous sur facebook" title="Facebook"></a>
+		<a href="www.instagram.com"><img src="../images/logo_instagram" alt="suivez-nous sur instagram" title="Instagram"></a>
+		<a href="www.pinterest.com"><img src="../images/logo_pinterest" alt="suivez-nous sur pinterest" title="Pinterest"></a>
+	</div>
+
+	<div id="footer_block_middle">
+		<h2>
 			En savoir plus
 		</h2>
 		<p> 
 			Nous sommes deux étudiants qui ont pour projet de créer un site d'<strong>e-commerce</strong>.
 			Nous souhaitons partager notre <strong>passion du manga</strong> en développant ce marché en France.
 			Fini le calvers pour trouver le bon <strong>tome</strong> que vous voulez. Enfin bref si vous voulez <strong>en savoir plus</strong> plus clique sur ce magnifique bouton !!!
-		</p>
-	</div>
-
-	<div id="footer_block_middle">
-		<p> 
-
 		</p>
 	</div>
 
