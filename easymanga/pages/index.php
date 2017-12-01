8a821fa894e072cffb44277d254d7b2385ec0334
Florian <florian.lafuente@ynov.com>
2017-12-01 10:26:39 +0100

    integration bootstrap

--- easymanga/pages/index.php
+++ easymanga/pages/index.php
@@ -3,8 +3,14 @@
     <head>        
         <title>easymanga</title>
         <meta charset="utf-8" />
+        
+        <!-- Pour Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+        
+        <!-- Google font et Feuille de style -->
         <link href="https://fonts.googleapis.com/css?family=Ubuntu|Poiret+One|Quicksand" rel="stylesheet">
         <link rel="stylesheet" href="../style/style.css">
+          
     </head>
     <body>
         
@@ -12,12 +18,25 @@
         
         <div id="fond_banniere">
             <div id="banniere">
-            
+                <div id="imgs-banniere">
+<!--
+                    <img ... />
+                    <img ... />
+-->
+                </div>
+                <div id="msgs-banniere">
+                    
+                </div>
             </div>
         </div>
 
         <?php include 'footer.php' ; ?>
 
 
+        
+        <!-- Pour Bootstrap ne aps bouger -->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
     </body>
 </html>
