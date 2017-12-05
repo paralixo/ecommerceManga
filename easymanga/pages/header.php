d1ba2f06cc567ea7f0b7493665ff865d1b9a3589
paralixo <33687200+paralixo@users.noreply.github.com>
2017-12-05 11:12:03 +0100

    Amélioration header
    Responsive OK mais problème de placement pour un élément quand l'écran est trop petit
    Il faut trouver/faire une image à la place du boutton que j'ai mis

--- easymanga/pages/header.php
+++ easymanga/pages/header.php
@@ -10,16 +10,17 @@
     <div class="row align-items-center">
         
         
-        <a href="index.php" alt="lien vers accueil depuis logo"  class="col-sm-1 logo">
+        <a href="index.php" alt="lien vers accueil depuis logo"  class="col-sm-1 logo align-self-start">
             <img class="logo" src="../images/renard.png" alt="logo easyManga" title="Notre magnifique logo"/>
         </a>
         
         <div class="col-sm">
             
             <h1 class="col-sm-12">easyManga</h1>
-
+            
+            
         
-            <nav class="col-sm-12">
+            <nav class="col-lg-12 collapse"  id="navbarToggleExternalContent">
                 <ul class="row justify-content-center">
                     <li class="col-lg-2 col-xl-2">
                         <a href="#">Produits dérivés</a>
@@ -36,19 +37,51 @@
                     <li class="col-lg-2 col-xl-2">
                         <a href="#">Mon Compte</a>
                     </li>
+                    <li class="col-md-12 d-lg-none d-xl-none">
+                        <a href="#">Mon Panier</a>
+                    </li>
+                </ul>
+            </nav>
+            
+            
+            <nav class="d-none d-lg-block d-xl-block col-lg-12 col-xl-12">
+                <ul class="row justify-content-center">
+                    <li class="col-lg-2 col-xl-2">
+                        <a href="#">Produits dérivés</a>
+                    </li>
+                    <li class="col-lg-2 col-xl-1">
+                        <a href="#">Mangas</a>
+                    </li>
+                    <li class="col-lg-2 col-xl-1">
+                        <a href="index.php" alt="lien vers l'accueil">Accueil</a>
+                    </li>
+                    <li class="col-lg-2 col-xl-1">
+                        <a href="contact.php" alt="lien vers formulaire de contact">Contact</a>
+                    </li>
+                    <li class="col-lg-2 col-xl-2">
+                        <a href="#">Mon Compte</a>
+                    </li>
+                    <li class="col-md-12 d-lg-none d-xl-none">
+                        <a href="#">Mon Panier</a>
+                    </li>
                 </ul>
             </nav>
             
         </div>
+        
+        <nav class="navbar navbar-dark bg-dark d-lg-none d-xl-none align-self-start">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
 
         
 
 
-        <a href="#" alt="lien vers mon panier" class="col-sm-1 panier">
+        <a href="#" alt="lien vers mon panier" class="d-none d-lg-block d-xl-block col-lg-1 col-xl-1 panier align-self-start">
             <img class="panier" src="../images/logo_caddie.png" alt="mon panier" title="Mon panier"/>
         </a>
         
         
     </div>
 </header>
-
